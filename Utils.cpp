696203faa9327f0720a469ecea520343d1cd4ce0
Akshay <akshaybabloo@gmail.com>
2021-05-11 11:27:18 +1200

    utils init

---
+++ Utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Akshay Raj Gollahalli on 11/05/21.
+//
+
+#include "Utils.h"
---
+++ Utils.h
@@ -0,0 +1,17 @@
+//
+// Created by Akshay Raj Gollahalli on 11/05/21.
+//
+
+#ifndef DATALOGGER_UTILS_H
+#define DATALOGGER_UTILS_H
+
+
+class Utils {
+
+public:
+
+
+};
+
+
+#endif //DATALOGGER_UTILS_H
