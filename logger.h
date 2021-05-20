8b95336dcc56e65360e3feb4c5ceb910121456c0
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-05-20 16:26:41 +1200

    controller UUID's added

--- logger.h
+++ logger.h
@@ -54,6 +54,18 @@ private:
     Ui::Logger *ui;
 
     bool randomAddress = false;
+
+    // data services where data can be streamed from the device
+    const QString ChannelDataServiceUUID = "5C872000-D20C-11EA-87D0-0242AC130003";
+    const QString ChannelsUUID = "5C872001-D20C-11EA-87D0-0242AC130003"; // Read/write = 3 bytes, 1 bit for each channel
+    const QString ChannelsSubscribeUUID = "5C872003-D20C-11EA-87D0-0242AC130003"; // Read/notify = 50 bytes. 2 bytes for msec counter. 48 bytes for data (2bytes/ch)
+
+    // device information and controllers
+    const QString DeviceControlServiceUUID = "5C873000-D20C-11EA-87D0-0242AC130003";
+    const QString TimestampUUID = "5C873001-D20C-11EA-87D0-0242AC130003"; // Write = see BLE protocol doc (to sync time with system)
+    const QString DeviceStatusUUID = "5C873002-D20C-11EA-87D0-0242AC130003"; // Read = see BLE protocol doc
+    const QString FormatSdCardUUID = "5C873003-D20C-11EA-87D0-0242AC130003"; // Write = 0x01 (to format the card)
+    const QString ADCLoggingUUID = "5C873004-D20C-11EA-87D0-0242AC130003"; // Read/Write = 0x01 (enable) or 0x00 (disable)
 };
 
 #endif // LOGGER_H
