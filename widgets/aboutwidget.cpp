3a3c5d2735e9c768a7525cd1b5ea99335ed3d12c
Akshay <akshaybabloo@gmail.com>
2021-11-25 13:14:56 +1300

    Bump to v2

--- widgets/aboutwidget.cpp
+++ widgets/aboutwidget.cpp
@@ -9,7 +9,7 @@ AboutWidget::AboutWidget(QWidget *parent) :
     QPixmap logo(":/images/datalogger.png");
     ui->logo->setPixmap(logo.scaledToHeight(100, Qt::SmoothTransformation));
 
-    ui->version->setText(tr("v1.0.0"));
+    ui->version->setText(tr("v2.0.0"));
 
     auto datetime = QStringLiteral(__DATE__) + QStringLiteral(" ") + QStringLiteral(__TIME__);
     ui->builtWith->setText(tr("Built with Qt v%1 on %2").arg(QT_VERSION_STR, datetime));
